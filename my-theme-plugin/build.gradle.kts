800714b8ed741cffb4292ac0634ba29f70289f85
Boris Zubarev <bobazooba@gmail.com>
2024-10-17 21:33:57 +0400

    Add theme and set up

--- my-theme-plugin/build.gradle.kts
+++ my-theme-plugin/build.gradle.kts
@@ -7,15 +7,12 @@ version = "0.1.0"
 
 fleetPlugin {
 
-    // STEP 1: chose an ID for your plugin, it must be unique
-    //
-    // id = "your.plugin.id.here"
+    id = "me.bobazooba.perf"
 
-    // STEP 2: set up the sensible metadata for your plugin
-    // metadata {
-    //     readableName = "Your plugin name here"
-    //     description = "Your plugin description here"
-    // }
+    metadata {
+        readableName = "Perf Theme"
+        description = "Red, Pink, Warm, No contrast, No visual noise. Like XCode, but better"
+    }
 
     fleetRuntime {
         version = libs.versions.fleet.runtime
